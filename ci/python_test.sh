53c1fd5e1e81201091b059e962c462d73ee62261
Jorge Blanco Alonso <jblanco@cbdhcp-2-003.epfl.ch>
2020-02-17 13:28:37 +0100

    Get latest virtualenv version

--- ci/python_test.sh
+++ ci/python_test.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 VENV=env
 
-pip install virtualenv
+python -m pip install virtualenv --upgrade
 rm -rf "$VENV"
 virtualenv "$VENV" --seeder=pip
 
@@ -12,9 +12,9 @@ set +u  # ignore errors in virtualenv's activate
 source "$VENV/bin/activate"
 set -u
 
-pip install --upgrade pip
+python -m pip install --upgrade pip
 
 # install
-pip install .
-pip install nose
+python -m pip install .
+python -m pip install nose
 nosetests python
